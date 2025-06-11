c2be1d79a9b798af43060a75000cedc30d25245e
nandan8983 <103305922+nandan8983@users.noreply.github.com>
2025-06-11 22:29:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const incrementBtn = document.getElementById("incrementBtn");
 
 incrementBtn.addEventListener("click", () => {
 	alert(counter.textContent); 
-    counter.textContent = parseInt(counter.textContent) + 1; 
+    counter.textContent = Number(counter.textContent) + 1; 
 });
